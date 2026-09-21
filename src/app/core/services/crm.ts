5a0e65474ddf0065a1342034a4dcbb2d0ca8199a
farrukhdev01 <farrukhdev01@gmail.com>
2026-09-21 20:22:14 +0500

    chore: leads crud

--- src/app/core/services/crm.ts
+++ src/app/core/services/crm.ts
@@ -33,7 +33,7 @@ export class CrmService {
 
   getSystemStatus() {
     return this.http.get<SystemStatusResponse>(
-      `${this.apiUrl}/system/status`
+      `${this.apiUrl}/status`
     );
   }
 
